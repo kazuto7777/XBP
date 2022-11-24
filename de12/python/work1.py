e9a96ea90a4b8509e0eb446e4cd314499ac933c5
kazuto7777 <r202202063dy@jindai.jp>
2022-11-24 14:04:29 +0900

    20221124

---
+++ de12/python/work1.py
@@ -0,0 +1,6 @@
+#ageという変数に44を代入している
+name="kazuto"
+waist=84
+age=44
+
+print(name, "さんは腹囲", waist, "cmで年齢は",age, "才ですね。")
---
+++ de12/python/work100.py
@@ -0,0 +1,5 @@
+hayaoki=input("早起きは得意ですか？")
+ienokyori=input("家は近いですか？")
+kadai=input("課題は早めにやりますか？")
+
+
